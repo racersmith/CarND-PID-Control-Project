c90c67859e5b63f7a8bcf5f3cacbf04e566c91be
Josh Smith <racersmith@gmail.com>
2017-06-03 10:42:08 -0700

    Faster tuning - Test termination on error and time - Improve first pass handling - Add comments

--- src/Twiddle.cpp
+++ src/Twiddle.cpp
@@ -9,28 +9,29 @@
 Twiddle::Twiddle() {}
 Twiddle::~Twiddle() {}
 
-void Twiddle::Init(double set_cte_offset, unsigned long int set_counter_max, unsigned long int set_settle_time, double dp_p, double dp_i, double dp_d) {
-	cte_offset = set_cte_offset;
-	error_time = set_counter_max;
-	wander_time = set_settle_time;
-
+void Twiddle::Init(unsigned long int error_time, unsigned long int wander_time, double wander_offset, double dp_p, double dp_i, double dp_d) {
+	// Set wander parameters	
+	this->cte_offset = wander_offset;
+	this->error_time = error_time;
+	this->wander_time = wander_time;
+	
 	// Set adjustment parameters
 	dp[0] = dp_p;
-	//dp[1] = dp_i;
-	//dp[2] = dp_d;
-	dp[1] = dp_d;
+	dp[1] = dp_i;
+	dp[2] = dp_d;
 	param_index = 0;
 
 	// Offset scalar, setting this to 0 will turn off twiddle
 	offset_state = 1;
 
 	// Set best error to negative as flag for first pass
-	running_error = 0;
-	best_error = -1;
+	running_error = 0.0;
+	best_error = -1.0;
 
 	// initialize test counter
-	error_counter = 0;
-	wander_counter = 0;
+	// negative values to add aditional time to first pass
+	error_counter = -500;
+	wander_counter = -500;
 
 	// initialize twiddle_state
 	twiddle_state = 0;
@@ -43,17 +44,23 @@ double Twiddle::RMSE() {
 }
 
 double Twiddle::Wander(double cte) {
-	wander_counter++;
-	if (wander_counter >= wander_time) {
-		wander_counter = 0;
-		offset_state *= -1;
+	if (wander_time > 0) {
+		wander_counter++;
+		if (wander_counter >= wander_time) {
+			wander_counter = 0;
+
+			offset_state *= -1;
+		}
+		return cte + offset_state*cte_offset;
 	}
-	return cte + offset_state*cte_offset;
+	else {
+		return cte;
+	}
 }
 
 double Twiddle::Tune(double cte, double& Kp, double& Ki, double& Kd) {
 	// Check if tuning is complete
-	double sum_dp = dp[0] + dp[1];// +dp[2];
+	double sum_dp = dp[0] + dp[1] + dp[2];
 	
 	if (sum_dp < 0.0001) {
 		// return cte unmodified to resume normal driving
@@ -61,45 +68,49 @@ double Twiddle::Tune(double cte, double& Kp, double& Ki, double& Kd) {
 		return cte;
 	}
 	
+	// Preturb the target position using wander
 	double adjusted_cte = Wander(cte);
-	// Add squared error to running error
+
 	running_error += adjusted_cte*adjusted_cte;
 
-	std::cout << "cte: " << adjusted_cte << "\tSum dp: " << sum_dp << "\tKp: " << Kp << "\tKi: " << Ki << "\tKd: " << Kd << "\tRun Err: " << running_error << std::endl;
+	// Debug printing
+	std::cout << "wander: " << offset_state << "\tcte: " << adjusted_cte << "\tSum dp: " << sum_dp << "\tKp: " << Kp << "\tKi: " << Ki ;
+	std::cout  << "\tKd: " << Kd  << "\tcounter: " << error_counter << "\tBest: " << best_error*best_error << "\tError: " << running_error << std::endl;
 	
+	// Check if evaluation time has elapsed or if the error has exceeded the best
 	error_counter++;
-	if (error_counter >= error_time) {
+	if (error_counter >= error_time || (running_error > 1.01 * best_error*best_error && best_error > 0.0)) {
 		error_counter = 0;
+		wander_counter = 0;
 
 		double error = RMSE();
 
-		// create array
-		double p[2];
+		double p[3];
 		p[0] = Kp;
-		//p[1] = Ki;
-		//p[2] = Kd;
-		p[1] = Kd;
-
-		int n_param = 2;
+		p[1] = Ki;
+		p[2] = Kd;
+		int n_param = 3;
 
 		// On first pass update to actual error
 		if (best_error == -1) {
 			best_error = error;
-			// test first case
+			// start first test
 			p[param_index] += dp[param_index];
 		}
-		// On improvement
+		// Improvement
 		else if (error < best_error) {
+			twiddle_state = 0;
 			best_error = error;
 			dp[param_index] *= 1.1;
-			twiddle_state = 0;
 			param_index = (param_index + 1) % n_param;
 			p[param_index] += dp[param_index];
 		}
-		// On no improvement
+		// No improvement
 		else {
 			switch (twiddle_state)
 			{
+			
+			// After first parameter test
 			case 0:
 				twiddle_state = 1;
 				// Check for calculation of negative param
@@ -110,23 +121,26 @@ double Twiddle::Tune(double cte, double& Kp, double& Ki, double& Kd) {
 					p[param_index] = 0.25 * p[param_index];
 				}
 				break;
+
+			// After second parameter test
 			case 1:
 				twiddle_state = 0;
+				p[param_index] += dp[param_index];
 				dp[param_index] *= 0.9;
 				param_index = (param_index + 1) % n_param;
 				p[param_index] += dp[param_index];
 				break;
+
+			// What are you doing down here?
 			default:
 				twiddle_state = 0;
 				param_index = 0;
 				break;
 			}
 		}
-
 		Kp = p[0];
-		//Ki = p[1];
-		//Kd = p[2];
-		Kd = p[1];
+		Ki = p[1];
+		Kd = p[2];
 	}
 	return adjusted_cte;
 }
--- src/Twiddle.h
+++ src/Twiddle.h
@@ -7,6 +7,10 @@
 #ifndef Twiddle_H
 #define Twiddle_H
 
+/*
+* Twiddle
+* Systematically adjust parameters to minimize error
+*/
 class Twiddle {
 private:
 	/*
@@ -29,23 +33,24 @@ private:
 	/*
 	* Errror measurement time
 	*/
-	unsigned long int error_counter;
-	unsigned long int error_time;
+	int error_counter;
+	int error_time;
 
 	/*
 	* Cross track error offset
 	*/
 	double cte_offset;
 	int offset_state;
-	unsigned long int wander_counter;
-	unsigned long int wander_time;
+	int wander_counter;
+	int wander_time;
 
 	/*
 	* Calculate the total PID error.
 	*/
 	double RMSE();
-
 	double Wander(double cte);
+
+	
 
 
 public:
@@ -60,12 +65,26 @@ public:
 	virtual ~Twiddle();
 
 	/*
-	* Initialize PID.
+	* Initialize Twiddle
+	* 
+	* @param error_time How long to trial a new parameter set
+	* @param wander_time How frequently to switch sides of road
+	* @param wander_offset How much to shift off centerline when wandering
+	* @param dp_p Initial adjustment size of Kp
+	* @param dp_i Initial adjustment size of Ki
+	* @param dp_d Initial adjustment size of Kd
 	*/
-	void Init(double set_cte_offset, unsigned long int set_counter_max, unsigned long int set_settle_time, double dp_p, double dp_i, double dp_d);
+	void Init(unsigned long int error_time, unsigned long int wander_time, double wander_offset, double dp_p, double dp_i, double dp_d);
 	
 	/*
 	* Tune the PID paramters
+	* 
+	* @param cte Current cross track error
+	* @param Kp Current Kp parameter
+	* @param Ki Current Ki parameter
+	* @param Kd Current Kd parameter
+
+	* @return Preturbed CTE from wander
 	*/
 	double Tune(double cte, double& Kp, double& Ki, double& Kd);
 };
